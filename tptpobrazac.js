9bce4d4d5689c0284f18af534c6ea57649ed8c60
Mahir Suljic <133034737+mahirsuljic-fet@users.noreply.github.com>
2023-05-31 15:10:54 +0200

    test

--- tptpobrazac.js
+++ tptpobrazac.js
@@ -30,45 +30,9 @@ function robot_check() {
     var robot_container = document.getElementById("robot-kontakt-container");
     var disable_div = document.getElementById("robot-disable-page");
 
-    var errormsg = document.getElementById("kontakt-validation-error");
-    var name = document.getElementById("kontakt-ime").value;
-    var lastname = document.getElementById("kontakt-prezime").value;
-    var email = document.getElementById("kontakt-email").value;
-    var kontaktvld = document.getElementById("info-kontakt");
-
-    errormsg.innerHTML = "";
-    kontaktvld.style.border = "0.25em solid rgba(0, 50, 100, 0.75)";
-    kontaktvld.style.animation = "";
-
-    // provjera unosa imena prazimena i e-maila
-    if(!name.match(/^[a-zA-Z]+$/) && name != "")
-        errormsg.innerHTML += "Ime nije validno! <br>";
-    if(!lastname.match(/^[a-zA-Z]+$/) && lastname != "")
-        errormsg.innerHTML += "Prezime nije validno! <br>";
-    if(!email.match(/^[a-z0-9]+@[a-z]+\.[a-z]{2,3}$/) && email != "")
-        errormsg.innerHTML += "E-mail nije validan! <br>";
-
-    if(errormsg.innerHTML != "")
-    {
-        kontaktvld.style.border = "0.25em solid red";
-        kontaktvld.style.animation = "shake 0.5s";
-        cb.checked = false;
-
-        var newelm = kontaktvld.cloneNode(true);
-        kontaktvld.parentNode.replaceChild(newelm, kontaktvld);
-
-        return;
-    }
-
     if (cb.checked) {
         i = Math.floor(Math.random() * (animals.length));
         document.getElementById("robot-image").src = "images/kontakt/" + animals[i] + ".jpg";
-
-        document.getElementById("robot-answer").addEventListener('keypress', function (e) {
-            console.log("Test");
-            if (e.key === 'Enter')
-                robot_answer();
-        });
 
         robot_container.style.display = "flex";
         disable_div.style.display = "block";
@@ -103,14 +67,6 @@ function robot_answer() {
 
         var newelm = robot_div.cloneNode(true);
         robot_div.parentNode.replaceChild(newelm, robot_div);
-        
-        document.getElementById("robot-answer").addEventListener('keypress', function (e) {
-            console.log("Test");
-            if (e.key === 'Enter')
-                robot_answer();
-        });
-
-        document.getElementById("robot-answer").focus();
     }
 }
 
